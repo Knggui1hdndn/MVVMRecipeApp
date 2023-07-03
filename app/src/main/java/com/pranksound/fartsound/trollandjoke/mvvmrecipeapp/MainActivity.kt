448747694fe31970fc933e82fbba28bf1a5690ef
DESKTOP-AV9M95Q\khangpc <khangnguyen3002@gmail.com>
2023-07-03 18:49:33 +0700

    o

--- app/src/main/java/com/pranksound/fartsound/trollandjoke/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/com/pranksound/fartsound/trollandjoke/mvvmrecipeapp/MainActivity.kt
@@ -5,13 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.pranksound.fartsound.trollandjoke.mvvmrecipeapp.ui.theme.MVVMRecipeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,10 +36,13 @@ private fun Content() {
         Image(
             painter = painterResource(id = R.drawable.happy_meal_small),
             contentDescription = "",
+            contentScale = ContentScale.FillBounds,
             modifier = Modifier
                 .fillMaxWidth()
-                .fillMaxHeight(0.4f)
+                .fillMaxHeight(0.3f)
         )
+        Spacer(modifier = Modifier.heightIn(15.dp))
+        Text(text = "Happy Meal", fontSize = 20.sp)
     }
 }
 
