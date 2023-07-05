b94cdba8963668d13dbc159c93f5e753d9f32dbf
Knggui1hdndn <khangndph20612@fpt.edu.vn>
2023-07-05 18:21:32 +0700

    ko

--- app/src/main/java/com/pranksound/fartsound/trollandjoke/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/com/pranksound/fartsound/trollandjoke/mvvmrecipeapp/MainActivity.kt
@@ -9,10 +9,15 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,26 +30,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Content()
-        }
+         }
     }
 
 }
 
 @Composable
 private fun Content() {
-    Column(modifier = Modifier.fillMaxSize()) {
+
+    Column(modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState())) {
         Image(
             painter = painterResource(id = R.drawable.happy_meal_small),
             contentDescription = "",
             contentScale = ContentScale.FillBounds,
             modifier = Modifier
                 .fillMaxWidth()
-                .fillMaxHeight(0.3f)
+                .height(300.dp)
         )
-        Spacer(modifier = Modifier.heightIn(15.dp))
-        Text(text = "Happy Meal", fontSize = 20.sp)
+        Column(modifier = Modifier.padding(10.dp)) {
+            Text(text = "Happy Meal", fontSize = 20.sp)
+            Text(text = "800 Calories", fontSize = 17.sp)
+            Text(text = "$5.99", fontSize = 17.sp, color = Color.Green)
+        }
     }
-}
+   }
 
 @Preview(showBackground = true)
 @Composable
